cd03780e8c73edf6714ba75dc12bfac15ed050ac
Christopher Girvin <bugbounty.chrisg8691@gmail.com>
2020-11-13 11:18:28 -0500

    axiosWithAuth and PrivateRoute done and added to app.js

---
+++ client/src/utils/axiosWithAuth.js
@@ -0,0 +1,10 @@
+import Axios from "axios";
+
+export const axiosWithAuth = () => {
+  return axios.create({
+    headers: {
+      Authorization: localStorage.getItem("token"),
+    },
+    baseURL: "http://localhost:5000",
+  });
+};
